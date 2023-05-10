3270284b78987c3e96480e1d5fb8382b0900b8a2
Danilo <didox_59@yahoo.com.br>
2023-05-10 11:02:24 -0300

    versionando correndo para nao atrapalha no tempo

---
+++ LabMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using LabWebForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.clientes = Cliente.Todos();
+            //ViewData["clientes ssds"] = Cliente.Todos();
+            return View(new
+            {
+                Clientes = Cliente.Todos(),
+                Mensagem = "oi"
+            });
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LabMVC/Models/Estado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LabWebForms.Models
+{
+    public class Estado
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string UF { get; set; }
+
+        private static string ConnectionString = ConfigurationManager.ConnectionStrings["MinhaConexao"].ConnectionString;
+
+        public override string ToString()
+        {
+            return this.Nome;
+        }
+
+        public static List<Estado> Todos()
+        {
+            List<Estado> estados = new List<Estado>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT id, nome, uf FROM Estados";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Estado estado = new Estado();
+                            estado.Id = reader.GetInt32(0);
+                            estado.Nome = reader.GetString(1);
+                            estado.UF = reader.GetString(2);
+                            estados.Add(estado);
+                        }
+                    }
+                }
+            }
+
+            return estados;
+        }
+    }
+}
