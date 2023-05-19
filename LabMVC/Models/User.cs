037ec527bd07d04dcd7c94e7724de581dc9de315
Charles Magalhaes <charlesrm29@gmail.com>
2023-05-19 10:22:45 -0300

    Criando Models/User.cs e criando tabela Users no postgre

---
+++ LabMVC/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabMVC.Models
+{
+    public class User
+    {
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
