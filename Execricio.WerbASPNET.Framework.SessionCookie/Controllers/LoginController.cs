2741f4d51f9f77bffc0d2c3bf50e7c33a4197ff5
rderoncio <rafael.deroncio@hotmail.com>
2023-05-20 10:53:41 -0300

    ajustes no login

--- Execricio.WerbASPNET.Framework.SessionCookie/Controllers/LoginController.cs
+++ Execricio.WerbASPNET.Framework.SessionCookie/Controllers/LoginController.cs
@@ -1,8 +1,4 @@
 ﻿using Execricio.WerbASPNET.Framework.SessionCookie.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Execricio.WerbASPNET.Framework.SessionCookie.Controllers
@@ -24,7 +20,7 @@ namespace Execricio.WerbASPNET.Framework.SessionCookie.Controllers
 
                 if (loginValido)
                 {
-                    ViewBag.UserLoged = $"Olá, {model.Username}";
+                    TempData["User"] = model.Username;
                     return RedirectToAction("Index", "Home");
                 }
                 else
