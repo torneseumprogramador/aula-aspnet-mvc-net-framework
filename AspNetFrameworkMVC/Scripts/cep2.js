14613919f380883fc3866144db883325f25f12c0
Cairo Braz Costa <cairo.costa@animaeducacao.com.br>
2023-05-19 21:18:31 -0300

    Adicionando validações para buscaCep

---
+++ AspNetFrameworkMVC/Scripts/cep2.js
@@ -0,0 +1,63 @@
+﻿
+$(document).ready(function () {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#rua").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+    }
+
+    //Quando o campo cep perde o foco.
+    $("input[name='cep']").change(function () {
+        //Nova variável "cep" somente com dígitos.
+        var numCep = $("#cep").val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (numCep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if (validacep.test(numCep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#rua").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                $("#ibge").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                var url = "https://viacep.com.br/ws/" + numCep + "/json/";
+
+                $.get(url, function (dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#rua").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
