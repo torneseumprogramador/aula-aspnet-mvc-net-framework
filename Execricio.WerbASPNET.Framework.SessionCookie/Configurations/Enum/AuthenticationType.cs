fe785bc13cdfa76f470df471a4bd3e7e98c48a21
rderoncio <rafael.deroncio@hotmail.com>
2023-05-20 10:38:01 -0300

    tela de login voltou

---
+++ Execricio.WerbASPNET.Framework.SessionCookie/Configurations/Enum/AuthenticationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Execricio.WerbASPNET.Framework.SessionCookie.Configurations.Enum
+{
+    public enum AuthenticationType
+    {
+        Session,
+        Cookie
+    }
+}
