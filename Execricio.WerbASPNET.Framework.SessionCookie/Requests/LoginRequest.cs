b74c6e7531797a035082ea861aef7ffdc9c80c3c
rderoncio <rafael.deroncio@hotmail.com>
2023-05-20 11:01:46 -0300

    Add requests, responses

---
+++ Execricio.WerbASPNET.Framework.SessionCookie/Requests/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using Execricio.WerbASPNET.Framework.SessionCookie.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Execricio.WerbASPNET.Framework.SessionCookie.Requests
+{
+    public class LoginRequest : AccessDTO
+    {
+    }
+}
---
+++ Execricio.WerbASPNET.Framework.SessionCookie/Responses/LoginResponse.cs
@@ -0,0 +1,12 @@
+﻿using Execricio.WerbASPNET.Framework.SessionCookie.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Execricio.WerbASPNET.Framework.SessionCookie.Responses
+{
+    public class LoginResponse : AccessDTO
+    {
+    }
+}
