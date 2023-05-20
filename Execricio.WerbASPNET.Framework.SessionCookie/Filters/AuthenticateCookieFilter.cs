b3734e947db8104143aa97108eccdaaee0dda05c
rderoncio <rafael.deroncio@hotmail.com>
2023-05-20 11:12:42 -0300

    Add filters

---
+++ Execricio.WerbASPNET.Framework.SessionCookie/Filters/AuthenticateCookieFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace Execricio.WerbASPNET.Framework.SessionCookie.Filters
+{
+
+    public class AuthenticateCookieFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.Cookies["AuthenticationCookie"] == null)
+                filterContext.Result = new HttpUnauthorizedResult();
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result is HttpUnauthorizedResult)
+                filterContext.Result = new RedirectResult("~/Login/Index");
+
+        }
+    }
+
+
+}
---
+++ Execricio.WerbASPNET.Framework.SessionCookie/Filters/AuthenticateSessionFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+
+namespace Execricio.WerbASPNET.Framework.SessionCookie.Filters
+{
+    public class AuthenticateSessionFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["Login"] == null)
+                filterContext.Result = new HttpUnauthorizedResult();
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result is HttpUnauthorizedResult)
+                filterContext.Result = new RedirectResult("~/Login/Index");
+        }
+    }
+
+}
