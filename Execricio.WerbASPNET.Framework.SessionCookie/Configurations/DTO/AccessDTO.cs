89584bafaa1940574f180aad77afefa3b53d22f6
rderoncio <rafael.deroncio@hotmail.com>
2023-05-20 00:19:02 -0300

    add dtos e enum

---
+++ Execricio.WerbASPNET.Framework.SessionCookie/Configurations/DTO/AccessDTO.cs
@@ -0,0 +1,16 @@
+﻿using Execricio.WerbASPNET.Framework.SessionCookie.Configurations.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Execricio.WerbASPNET.Framework.SessionCookie.Configurations
+{
+    public class AccessDTO
+    {
+        public string Username { get; set; }
+        public string PasswordHashed { get; set; }
+        public bool Sucess { get; set; }
+        public AuthenticationType Authentication { get; set; }
+    }
+}
