982aadc56e6b6137b32f8ec0191c68e6853fd394
rderoncio <rafael.deroncio@hotmail.com>
2023-05-20 10:31:28 -0300

    fiz cagada. corrigindo ....

--- Execricio.WerbASPNET.Framework.SessionCookie/Controllers/HomeController.cs
+++ Execricio.WerbASPNET.Framework.SessionCookie/Controllers/HomeController.cs
@@ -10,9 +10,21 @@ namespace Execricio.WerbASPNET.Framework.SessionCookie.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home";
             return View();
         }
 
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
